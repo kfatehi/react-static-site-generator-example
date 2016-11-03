06bd0c37a5e7c1a1fdae67d91a84bdc2cddae3ae
Keyvan Fatehi <keyvanfatehi@gmail.com>
2016-11-02 22:41:49 -0700

    cleanup

--- bin/build-templates.js
+++ bin/build-templates.js
@@ -1,14 +1,27 @@
 #!/usr/bin/env node
-const util = require('../src/util');
+const fs = require('fs');
+const path = require('path');
 const Promise = require('bluebird');
 const config = require('../config.js');
-const path = require('path');
+const glob = Promise.promisify(require('glob'));
+const ExpressReactViews = require('express-react-views').createEngine;
+const writeFile = Promise.promisify(fs.writeFile);
 
-util.createRenderer(config.templateRoot).then(render => {
+// Resolves with a function taking a template filename (in templateRoot) 
+// and parameters (React props) and resolving the HTML
+function createRenderer(templateRoot) {
+  return glob(templateRoot+'/**/*.jsx').then(views => (name, params) => {
+    var render = Promise.promisify(ExpressReactViews({ beautify: true }))
+    var renderOptions = Object.assign({ settings: { views } }, params);
+    return render(path.join(templateRoot, name), renderOptions)
+  })
+}
+
+createRenderer(config.templateRoot).then(render => {
   return Promise.map(config.buildTemplates, item => {
     return render(item.template, item.params).then(html => {
       const out = path.join(config.htmlRoot, item.output);
-      return util.writeFile(out, html).then(() =>
+      return writeFile(out, html).then(() =>
         console.log('wrote '+path.basename(out)))
     })
   })
--- items.js
+++ items.js
@@ -15,11 +15,26 @@ module.exports = [{
   Crumple is a failed startup of mine in which we created an
   electronic receipt solution. It worked by intercepting the
   communication between a point of sale computer and a receipt
-  printer. We developed all the hardware and software for the
+  printer. The customer then claimed the receipt "over the air"
+  using a smartphone with our app installed -- or simply entered
+  a phone number or email address. We developed all the hardware
+  and software for the
   solution and even got it FCC certified. We had it deployed
   successfully in two different stores, but the sales "team"
   which was essentially just my partner, burned out and we
   closed up shop. A post-mortem is published at my blog, at this
   <a href="http://keyvanfatehi.com/2016/01/19/Crumple-a-post-mortem/">direct link</a>.
+  `
+},{
+  name: 'React Midterm',
+  imgSrc: '/img/placeholder.jpg',
+  description: `
+  This website is pretty cool, so I have included it here
+  in my portfolio. The graphic design may not be so great
+  but the technical statement it is making is important. Too
+  many websites now require JavaScript and won't even render
+  without it. This website uses JavaScript, heavily, but it
+  will render exactly the same on a server with or without
+  JavaScript enabled.
   `
 }]
